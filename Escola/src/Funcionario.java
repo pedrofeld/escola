0752381f8aa72bffdcf3b6cda8044fb1b509fb34
Pedro Feld <pedro.gael.feld@gmail.com>
2025-11-20 12:55:59 -0300

    feat: adicionando objeto Funcionario

---
+++ Escola/src/Funcionario.java
@@ -0,0 +1,11 @@
+public class Funcionario {
+	String nome;
+	String funcao;
+	int idade;
+	static final double salario = 1600.95;
+	// final define que esse valor não pode ser alterado
+
+}
+// classe sem boas práticas 
+// não informando a privacidade dos atributos
+// nem getters e setters
