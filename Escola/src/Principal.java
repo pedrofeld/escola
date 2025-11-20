4f067d7ffd1f71f7853186a3056f9fa9b0d99f85
Pedro Feld <pedro.gael.feld@gmail.com>
2025-11-20 12:58:52 -0300

    feat: instanciando um funcionario

--- Escola/src/Principal.java
+++ Escola/src/Principal.java
@@ -18,5 +18,31 @@ public class Principal {
         a.setNome("Pedro Gael Feld");
                 
         System.out.println("Nome atualizado: " + a.getNome());
+        
+        Funcionario f = new Funcionario();
+        f.nome = "Augusto Carrara";
+        f.idade = 61;
+        f.funcao = "Zelador";
+        
+        System.out.println("Nome do funcionário: " + f.nome);
+        System.out.println("Idade do funcionário: " + f.idade);
+        System.out.println("Função do funcionário: " + f.funcao);
+        System.out.println("Salário do funcionário" + f.salario);
     }
 }
+
+/*
+	Dentro do Java, os padrões são os seguintes:
+	
+	• Pacotes: são descritos inteiramente em letras minúsculas. 
+	Ex: com.empresa
+	
+	• Classes: inicia com letra maiúscula e segue o Camel Case. 
+	Ex: Aluno
+	
+	• Métodos, atributos e variáveis: iniciam com letra minúscula e seguem o Camel Case. 
+	Ex: nomeCompleto
+	
+	• Constantes: inteiramente com letras maiúsculas separadas por underline. 
+	Ex: VALOR_PI
+*/
