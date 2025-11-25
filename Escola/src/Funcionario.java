3447920ead950b696e007defac84ece29ba4061c
Pedro Feld <pedro.gael.feld@gmail.com>
2025-11-24 21:32:23 -0300

    feat: definindo tipos de construtores

--- Escola/src/Funcionario.java
+++ Escola/src/Funcionario.java
@@ -4,6 +4,24 @@ public class Funcionario {
 	int idade;
 	static final double salario = 1600.95;
 	// final define que esse valor não pode ser alterado
+	
+	// Construtor vazio
+	Funcionario() {}
+	
+	// Construtor com parâmetros
+	Funcionario(String pNome, String pFuncao, int pIdade){
+		nome = pNome;
+		funcao = pFuncao;
+		idade = pIdade;
+	}
+	
+	/*
+	// Se for passado somente a função, o nome puxa "Vazio" e idade puxa 0 como padrões
+	Funcionario(String funcao){
+		this.nome = "Vazio";
+		this.idade = 0;
+	}
+	*/
 
 }
 // classe sem boas práticas 
